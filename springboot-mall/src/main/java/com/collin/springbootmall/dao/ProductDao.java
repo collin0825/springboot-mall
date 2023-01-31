c9a359a6600015f45fb0f7ef651eb2b5687fe7c9
collin <collin0825@gmail.com>
2023-01-31 13:50:22 +0800

    商品功能-實作修改商品

--- springboot-mall/src/main/java/com/collin/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/collin/springbootmall/dao/ProductDao.java
@@ -8,4 +8,6 @@ public interface ProductDao {
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
+
+    void updateProduct(Integer productId, ProductRequest productRequest);
 }
