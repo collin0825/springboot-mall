ea0a2bdb6ed841bf76d786bebcec5ff69186cfdf
collin <collin0825@gmail.com>
2023-01-31 15:55:34 +0800

    商品功能-實作查詢商品列表 (基礎實作)

--- springboot-mall/src/main/java/com/collin/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/collin/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.collin.springbootmall.dao;
 import com.collin.springbootmall.dto.ProductRequest;
 import com.collin.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- springboot-mall/src/main/java/com/collin/springbootmall/service/impl/ProductServiceImpl.java
+++ springboot-mall/src/main/java/com/collin/springbootmall/service/impl/ProductServiceImpl.java
@@ -7,12 +7,19 @@ import com.collin.springbootmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class ProductServiceImpl implements ProductService {
 
 
     @Autowired
     private ProductDao productDao;
+
+    @Override
+    public List<Product> getProducts() {
+        return productDao.getProducts();
+    }
 
     @Override
     public Product getProductById(Integer productId) {
