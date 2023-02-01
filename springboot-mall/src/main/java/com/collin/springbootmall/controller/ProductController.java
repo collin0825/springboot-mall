4d4134c87f1de2e19a2ad04936a33e7e3dfc8c70
collin <collin0825@gmail.com>
2023-02-01 12:04:26 +0800

    商品功能-實作查詢商品列表 (分頁 + 總筆數)

--- springboot-mall/src/main/java/com/collin/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/collin/springbootmall/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.collin.springbootmall.dto.ProductQueryParams;
 import com.collin.springbootmall.dto.ProductRequest;
 import com.collin.springbootmall.model.Product;
 import com.collin.springbootmall.service.ProductService;
+import com.collin.springbootmall.util.Page;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -24,7 +25,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(
+    public ResponseEntity<Page<Product>> getProducts(
             // 查詢條件 Filtering
             @RequestParam(required = false) ProductCategory category,
             @RequestParam(required = false) String search,
@@ -45,9 +46,20 @@ public class ProductController {
         productQueryParams.setLimit(limit);
         productQueryParams.setOffset(offset);
 
+        // 取得 product list
         List<Product> productList = productService.getProducts(productQueryParams);
 
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+        // 取得 product 總數
+        Integer total = productService.countProduct(productQueryParams);
+
+        // 分頁
+        Page<Product> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResults(productList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
     @GetMapping("/products/{productId}")
