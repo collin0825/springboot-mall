0aa979426bcc5a01650ce3986c07c7fc2b54d238
collin <collin0825@gmail.com>
2023-02-01 15:01:52 +0800

    帳號功能-檢查註冊的 email、隱藏返回的密碼

--- springboot-mall/src/main/java/com/collin/springbootmall/dto/UserRegisterRequest.java
+++ springboot-mall/src/main/java/com/collin/springbootmall/dto/UserRegisterRequest.java
@@ -1,10 +1,12 @@
 package com.collin.springbootmall.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public class UserRegisterRequest {
 
     @NotBlank
+    @Email
     private String email;
 
     @NotBlank
